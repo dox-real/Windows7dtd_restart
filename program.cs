68bfdb77335c82bf2893dfe1768127390e1770f1
Dox <52359010+dox-real@users.noreply.github.com>
2020-07-22 19:51:45 -0400

    Update program.cs

--- program.cs
+++ program.cs
@@ -76,28 +76,30 @@ namespace Program
                         Console.WriteLine("The specified directory does not exist. {0}", e);
                     }
                     pathcomplete = direcname + "\\startdedicated.bat";
-                    Thread.Sleep(5000);
+                    
                     Console.Clear();
                     Console.WriteLine(pathcomplete);
+                    Console.WriteLine("Server located.");
+                    Thread.Sleep(2000);
                     PathServ.pathserv2 = pathcomplete;
-                    Thread.Sleep(5000);
+                    
                     Console.Clear();
                     
-                    Process.Start(pathcomplete); 
+                    Process.Start(pathcomplete);
                     Thread.Sleep(10000);
                 }
                 catch (System.IndexOutOfRangeException e)
                 {
                     Console.Clear();
-                    Console.WriteLine("Server does not appear to be running.");
+                    
                     try
                     {
                         Process.Start(PathServ.pathserv2);
                     }
                     catch { }
-                    Console.WriteLine("This would be a good place to try and start the server using a defined path.");
-                    Console.WriteLine("Please launch with server running. Trying again in 4 seconds.");
-                    Thread.Sleep(4000);
+                    Console.WriteLine("Please start server. This program uses the process to track the working directory.");
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadKey();
 
                 }
                 
