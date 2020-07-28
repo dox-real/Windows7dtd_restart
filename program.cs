8663f3e12c6c33698836852105447f68962ac489
Dox <52359010+dox-real@users.noreply.github.com>
2020-07-28 14:39:01 -0400

    working code

--- program.cs
+++ program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading;
 using System.Diagnostics;
 using System.IO;
+using System.Text;
 
 namespace Program
 {
@@ -16,12 +17,57 @@ namespace Program
     {
             
             var serverpath = (String)null;
+            var mepath = (String)null;
+            int configstart = 1;
+            string cfgfile = @"config.txt";
+            string cfgfile2 = @"config.txt";
+            string linetoWrite = null;
             string direcname;
+            string medirpath;
             string pathcomplete;
+            string pathtelnet = "cfg/telnet.bat";
             int on = 1;
+            int restarttimer = 28800000;
+
+            Console.WriteLine(File.Exists(cfgfile) ? "File exists." : "File does not exist.");
+            if (File.Exists(cfgfile))
+            {
+                Console.WriteLine("Config found.");
+            }
+            else
+            {
+                Console.WriteLine("Config not found.");
+                Thread.Sleep(2000);
+                Console.WriteLine("generating..");
+                using FileStream fs = File.Create(cfgfile);
+                Byte[] title = new UTF8Encoding(true).GetBytes("Dox's Windows Simple Restart." + Environment.NewLine);
+                fs.Write(title, 0, title.Length);
+
+            }
             while (on == 1)
             {
+                var me = System.Diagnostics.Process.GetProcessesByName("7dtd_SimpleStart")[0];
+                mepath = me.MainModule.FileName;
+                medirpath = Path.GetDirectoryName(mepath);
+
+
                 Console.Clear();
+                try
+                {
+                    using (StreamReader sr = File.OpenText(cfgfile))
+                    {
+                        string s = "";
+                        while ((s = sr.ReadLine()) != null)
+                            Console.WriteLine(s);
+                    }
+                }
+
+                catch (Exception Ex)
+                {
+                    Console.WriteLine(Ex.ToString());
+                }
+                
+                Thread.Sleep(4000);
                 Console.Write("Identify Server Process.");
                 Thread.Sleep(1000);
                 Console.Clear();
@@ -44,12 +90,19 @@ namespace Program
                     if (server != null)
                     {
                         serverpath = server.MainModule.FileName;
+                        using FileStream fs = File.OpenWrite(cfgfile);
+                        Byte[] title = new UTF8Encoding(true).GetBytes("Dox's Windows Simple Start" + Environment.NewLine + "Serverpath:" + serverpath + Environment.NewLine + "RestartTimer(Milliseconds): " + restarttimer +Environment.NewLine);
+                        fs.Write(title, 0, title.Length);
                         Console.Clear();
                         Console.Write("Path set to var, waiting..");
 
                         Console.Clear();
-                        Console.Write(server);
+                        Console.Write(server +Environment.NewLine);
+                        Console.Write("Restart in 5 seconds.");
                         Thread.Sleep(5000);
+                        Directory.SetCurrentDirectory(medirpath);
+                        Process.Start(pathtelnet);
+                        Thread.Sleep(28800000);
                         server.Kill();
                         Console.Clear();
                         Console.Write("attempted to kill server, waiting..");
@@ -97,7 +150,7 @@ namespace Program
                         Process.Start(PathServ.pathserv2);
                     }
                     catch { }
-                    Console.WriteLine("Please start server. This program uses the process to track the working directory.");
+                    Console.WriteLine(" For first time use please start server. This program uses the process to track the working directory.");
                     Console.WriteLine("Press any key to continue.");
                     Console.ReadKey();
 
@@ -110,3 +163,4 @@ namespace Program
 
    
 }
+
